27754408090594b34353d06605a29840db1b7690
Hoan Tran <hoantran.it@gmail.com>
2017-07-19 17:03:49 +0700

    Update service to put object from AWS S3 bucket

--- aws-service/src/main/java/com/hoantran/s3/S3Service.java
+++ aws-service/src/main/java/com/hoantran/s3/S3Service.java
@@ -6,8 +6,10 @@
  */
 package com.hoantran.s3;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,13 +18,21 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsV2Request;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 import com.amazonaws.util.StringUtils;
 
 /**
@@ -33,21 +43,35 @@ public class S3Service {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(S3Service.class);
 
-    private String accessKeyId;
+    private AmazonS3 s3Client;
 
-    private String secretAccessKey;
+    public S3Service() {
+        this.s3Client = getS3Connection();
+    }
 
-    /**
-     * @param accessKeyId
-     * @param secretAccessKey
-     */
+    public S3Service(String profile) {
+        this.s3Client = getS3Connection(profile);
+    }
+
     public S3Service(String accessKeyId, String secretAccessKey) {
-        super();
-        this.accessKeyId = accessKeyId;
-        this.secretAccessKey = secretAccessKey;
+        this.s3Client = getS3Connection(accessKeyId, secretAccessKey);
     }
 
     private AmazonS3 getS3Connection() {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+        return s3Client;
+    }
+
+    private AmazonS3 getS3Connection(String profile) {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider(profile))
+                .build();
+        return s3Client;
+    }
+
+    private AmazonS3 getS3Connection(String accessKeyId, String secretAccessKey) {
         AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
         return new AmazonS3Client(credentials);
     }
@@ -69,14 +93,13 @@ public class S3Service {
         return true;
     }
 
-    public List<S3Object> getS3Object(String bucketName) {
-        return getS3Object(bucketName, null);
+    public List<S3Object> getS3ObjectList(String bucketName) {
+        return getS3ObjectList(bucketName, null);
     }
 
-    public List<S3Object> getS3Object(String bucketName, String directoryPath) {
+    public List<S3Object> getS3ObjectList(String bucketName, String directoryPath) {
         List<S3Object> s3ObjectList = new ArrayList<S3Object>();
         try {
-            AmazonS3 s3Client = getS3Connection();
             ListObjectsV2Request req = new ListObjectsV2Request()
                     .withBucketName(bucketName);
             if (!StringUtils.isNullOrEmpty(directoryPath)) {
@@ -108,5 +131,42 @@ public class S3Service {
             LOGGER.warn("Error Message: {}", ace.getMessage());
         }
         return s3ObjectList;
+    }
+
+    public S3Object getS3Object(String bucket, String key) {
+        S3Object s3object = null;
+        try {
+            s3object = s3Client.getObject(new GetObjectRequest(
+                    bucket, key));
+        } catch (AmazonServiceException ase) {
+            LOGGER.warn("Caught an AmazonServiceException, which means your request made it to Amazon S3, but was rejected with an error response for some reason.");
+            LOGGER.warn("Error Message: {}", ase.getMessage());
+            LOGGER.warn("HTTP Status Code: {}", ase.getStatusCode());
+            LOGGER.warn("AWS Error Code: {}", ase.getErrorCode());
+            LOGGER.warn("Error Type: {}", ase.getErrorType());
+            LOGGER.warn("Request ID: {}", ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            LOGGER.warn("Caught an AmazonClientException, which means the client encountered an internal error while trying to communicate with S3, such as not being able to access the network.");
+            LOGGER.warn("Error Message: {}", ace.getMessage());
+        }
+        return s3object;
+    }
+
+    public InputStream getS3ObjectFile(String bucket, String key) {
+        return getS3Object(bucket, key).getObjectContent();
+    }
+
+    public void putS3ObjectFile(String bucket, String key, InputStream input, Map<String, String> userMetaData) {
+        TransferManager tm = TransferManagerBuilder.defaultTransferManager();
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setUserMetadata(userMetaData);
+        PutObjectRequest request = new PutObjectRequest(bucket, key, input, metadata);
+        Upload upload = tm.upload(request);
+        try {
+            upload.waitForCompletion();
+        } catch (AmazonClientException | InterruptedException amazonClientException) {
+            LOGGER.error("Unable to upload file, upload aborted.");
+            amazonClientException.printStackTrace();
+        }
     }
 }
--- aws-service/src/test/java/com/hoantran/s3/S3ServiceTest.java
+++ aws-service/src/test/java/com/hoantran/s3/S3ServiceTest.java
@@ -6,6 +6,9 @@
  */
 package com.hoantran.s3;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,36 +25,54 @@ public class S3ServiceTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(S3ServiceTest.class);
 
-    private final String accessKeyId = "AKIAI4TSWJGBJAI6SO7Q";
+    private final String bucketName = "i-think";
 
-    private final String secretAccessKey = "NPHivGXcLJftQWqCNZVdr7IX/oYYs/C4CafEOaRz";
+    private final String objectKey = "images/contributor/avatar.jpg";
 
-    private final String bucketName = "aws-service-demo";
+    private final String uploadKey = "images/text.txt";
+
+    private final String sampleText = "AWS sample text";
 
     /**
      * We have to have slash (/) at the end because aws use it to identify it is folder or not
      */
-    private final String directoryPath = "image/";
+    private final String directoryPath = "now-platform/";
 
     @Test
-    public void getS3Object() {
-        S3Service s3Service = new S3Service(accessKeyId, secretAccessKey);
-        List<S3Object> s3ObjectList = s3Service.getS3Object(bucketName);
+    public void getS3ObjectList() {
+        S3Service s3Service = new S3Service();
+        List<S3Object> s3ObjectList = s3Service.getS3ObjectList(bucketName);
         LOGGER.info("Print S3 object list inside {} bucket", bucketName);
-        printOutS3Object(s3ObjectList);
+        printOutS3ObjectKey(s3ObjectList);
     }
     
     @Test
-    public void getS3ObjectInsideFolder() {
-        S3Service s3Service = new S3Service(accessKeyId, secretAccessKey);
-        List<S3Object> s3ObjectList = s3Service.getS3Object(bucketName, directoryPath);
+    public void getS3ObjectListInsideFolder() {
+        S3Service s3Service = new S3Service();
+        List<S3Object> s3ObjectList = s3Service.getS3ObjectList(bucketName, directoryPath);
         LOGGER.info("Print S3 object list inside {} bucket, folder {}", bucketName, directoryPath);
-        printOutS3Object(s3ObjectList);
+        printOutS3ObjectKey(s3ObjectList);
     }
 
-    private void printOutS3Object(List<S3Object> s3ObjectList) {
+    @Test
+    public void getS3Object() {
+        S3Service s3Service = new S3Service();
+        S3Object s3Object = s3Service.getS3Object(bucketName, objectKey);
+        LOGGER.info("Print S3 object with key {} inside {} bucket", bucketName, objectKey);
+        printOutS3ObjectKey(Arrays.asList(s3Object));
+    }
+
+    @Test
+    public void putS3ObjectFile() {
+        S3Service s3Service = new S3Service();
+        InputStream input = new ByteArrayInputStream(sampleText.getBytes());
+        s3Service.putS3ObjectFile(bucketName, uploadKey, input, null);
+    }
+
+    private void printOutS3ObjectKey(List<S3Object> s3ObjectList) {
         for(S3Object s3Object:s3ObjectList){
             LOGGER.info("Object key: {}", s3Object.getKey());
         }
     }
+
 }
