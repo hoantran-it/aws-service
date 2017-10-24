99a205bba74be832f78b9779f177aef7b43bfa0d
Hoan Tran <hoantran.it@gmail.com>
2017-10-24 21:19:42 +0700

    Update SES service

---
+++ aws-service/src/main/java/com/github/hoantran/lib/aws/s3/SESService.java
@@ -0,0 +1,55 @@
+/**
+ * Created by Hoan Tran @ http://hoantran-it.blogspot.com
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ */
+package com.github.hoantran.lib.aws.s3;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+
+/**
+ * @author hoan.tran
+ */
+public class SESService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SESService.class);
+
+    public boolean sendMail(String fromEmail, String toEmail, String subject, String htmlBody, String textBody) throws IOException {
+        try {
+            AmazonSimpleEmailService client =
+                    AmazonSimpleEmailServiceClientBuilder.standard()
+                            .withRegion(Regions.US_WEST_2).build();
+            SendEmailRequest request = new SendEmailRequest()
+                    .withSource(fromEmail)
+                    .withDestination(new Destination().withToAddresses(toEmail))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withHtml(new Content()
+                                            .withCharset("UTF-8").withData(htmlBody))
+                                    .withText(new Content()
+                                            .withCharset("UTF-8").withData(textBody)))
+                            .withSubject(new Content()
+                                    .withCharset("UTF-8").withData(subject)));
+            client.sendEmail(request);
+            LOGGER.info("Email sent!");
+            return true;
+        } catch (Exception ex) {
+            LOGGER.error("The email was not sent. Error message: {}", ex.getMessage());
+            return false;
+        }
+    }
+
+}
---
+++ aws-service/src/test/java/com/github/hoantran/lib/aws/s3/SESServiceTest.java
@@ -0,0 +1,50 @@
+/**
+ * Created by Hoan Tran @ http://hoantran-it.blogspot.com
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ */
+package com.github.hoantran.lib.aws.s3;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ * @author hoan.tran
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SESServiceTest {
+
+    private static SESService sesService;
+
+    static final String FROM = "sender@example.com";
+
+    static final String TO = "recipient@example.com";
+
+    static final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+
+    static final String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+            + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+            + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+            + "AWS SDK for Java</a>";
+
+    static final String TEXTBODY = "This email was sent through Amazon SES "
+            + "using the AWS SDK for Java.";
+
+    @BeforeClass
+    public static void initialize() {
+        sesService = new SESService();
+    }
+
+    @Test
+    public void test_01_sendMail() throws IOException {
+        assertTrue(sesService.sendMail(FROM, TO, SUBJECT, HTMLBODY, TEXTBODY));
+    }
+
+}
