a7fd33d0fec923c9fcb8db1b93bb5954684c6316
Hoan Tran <hoantran.it@gmail.com>
2018-05-18 16:10:46 +0700

    Update EC2 service: check ami existence

---
+++ aws-service/src/main/java/com/github/hoantran/lib/aws/s3/EC2Service.java
@@ -0,0 +1,68 @@
+/**
+ * Created by Hoan Tran @ http://hoantran-it.blogspot.com
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ */
+package com.github.hoantran.lib.aws.s3;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeImagesRequest;
+import com.amazonaws.services.ec2.model.DescribeImagesResult;
+import com.amazonaws.services.ec2.model.Filter;
+
+/**
+ * @author hoan.tran
+ */
+public class EC2Service {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EC2Service.class);
+
+    private AmazonEC2 ec2Client;
+
+    public EC2Service() {
+        this.ec2Client = getEC2Connection();
+    }
+
+    public EC2Service(String profile) {
+        this.ec2Client = getEC2Connection(profile);
+    }
+
+    private AmazonEC2 getEC2Connection() {
+        AmazonEC2 ec2Client = AmazonEC2ClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+        return ec2Client;
+    }
+
+    private AmazonEC2 getEC2Connection(String profile) {
+        AmazonEC2 ec2Client = AmazonEC2ClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider(profile))
+                .build();
+        return ec2Client;
+    }
+
+    public boolean isAMIExist(Map<String, List<String>> tagValueMap) throws Exception {
+        final DescribeImagesRequest request = new DescribeImagesRequest();
+        for (Map.Entry<String, List<String>> tagValue : tagValueMap.entrySet()) {
+            request.withFilters(new Filter().withName(tagValue.getKey()).withValues(tagValue.getValue()));
+        }
+        try {
+            final DescribeImagesResult result = ec2Client.describeImages(request);
+            return result.getImages().size() > 0;
+        } catch (final Exception e) {
+            LOGGER.error("Error when checking ami existence", e);
+            return false;
+        }
+    }
+
+}
---
+++ aws-service/src/test/java/com/github/hoantran/lib/aws/s3/EC2ServiceTest.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Hoan Tran @ http://hoantran-it.blogspot.com
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ */
+package com.github.hoantran.lib.aws.s3;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author hoan.tran
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EC2ServiceTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EC2ServiceTest.class);
+
+    private static EC2Service ec2Service;
+
+    @BeforeClass
+    public static void initialize() {
+        ec2Service = new EC2Service();
+    }
+
+    @Test
+    public void test_01_isAMIExist() throws Exception {
+        HashMap<String, List<String>> tagValueMap = new HashMap<String, List<String>>();
+        tagValueMap.put("name", Arrays.asList("jenkins-ami"));
+        assertTrue(ec2Service.isAMIExist(tagValueMap));
+    }
+
+}
